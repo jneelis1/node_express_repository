fd9deb13e2b44cbb64413c8e2d925c6f89930966
mneel <neelismi@msu.edu>
2020-10-08 15:19:45 -0400

    New RDS Reader Version

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-const express = require ('express');
-const app = express ();
-
-app.get('/',(req,res)=>{
-    res.send("Welcome to the home page!");
-});
-
+const express = require('express')
+const app = express()
 const port = process.env.port || 3000;
-app.listen(port,()=>{
-    console.log("Wassapppp");
+
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: "web-scraper.c78qxjw1gccg.us-east-2.rds.amazonaws.com",
+    user: "admin",
+    password: "Capstone2020"
 });
+
+
+
+app.get('', (req, res) => {
+    con.connect(function(err) {
+        con.query(`SELECT * FROM Capstone.scrapes`, function(err, result, fields) {
+            if (err) res.send(err);
+            if (result) res.send(result[0]["textfound"]);
+        });
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+
+;
